e62b9fe5e4c456fbcda7dce94951ae9f932800d5
Alexandru Paval <pavalalexandruvlad@gmail.com>
2021-01-14 16:26:47 +0200

    deleted comments

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,11 +1,5 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-
-// one-line comment
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
