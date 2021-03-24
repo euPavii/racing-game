0fa271a98869225342d90f82219ce67724af46fd
Alexandru Paval <pavalalexandruvlad@gmail.com>
2021-03-24 12:05:37 +0200

    studied inheritance

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,17 +1,12 @@
 package org.fasttrackit;
 
-public class Car {
-    // instance variables
-    String name = "test";
-    double fuelLevel;
-    double mileage;
-    double totalTraveledDistance;
-    double maxSpeed;
+// "is-a" relationship (inheritance) - mostenire
+public class Car extends AutoVehicle {
 
     int doorCount;
-    boolean damaged;
-    String color;
 
-    // has-a relationship (aggregation)
-    Engine engine;
 }
+
+// shift+shift - search
+// nu putem mosteni direct de la 2 clase deodata, dar putem indirect
+// mostenirea ne ajuta sa refolosim bucati de cod
